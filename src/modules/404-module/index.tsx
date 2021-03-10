4aea2242da7bac5f8735eb3deeb33f6f0c4eeab1
Arnaud <cynapsenjoh@gmail.com>
2021-03-10 12:58:19 +0300

    test of auto builds

--- src/modules/404-module/index.tsx
+++ src/modules/404-module/index.tsx
@@ -4,7 +4,7 @@ import {My404Container} from "./404-style/default";
 const My404UI: React.FC = () => {
 
     return <My404Container>
-        404 - Module not found
+        404 - Module not found…
     </My404Container>;
 }
 
